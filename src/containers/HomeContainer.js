7477b0e8350e78a9446e80152848c410b5d6cb40
James Sode <ayindejam2009@yahoo.com>
2024-01-22 23:14:35 +0000

    returned h2 headings above buttons, modified css and updated wireframe

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -5,12 +5,19 @@ const HomeContainer = () => {
     <div className="home-container">
       <h1>Welcome to Can You Do It?</h1>
       <div className="button-container">
-        <button className="big-button">Discover and Apply for Jobs</button>
-        <button className="big-button">Post Jobs and View Applicants</button>
+        <div className="role-container">
+          <h2>I'm a Candidate</h2>
+          <button className="big-button">Discover and Apply for Jobs</button>
+        </div>
+        <div className="role-container">
+          <h2>I'm an Employer</h2>
+          <button className="big-button">Post Jobs and View Applicants</button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default HomeContainer;
+
 
